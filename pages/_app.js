e29ebdb16c9fab6bc81f05d17dd54b509becb339
renatamellodia-dotcom <renatamellodia@gmail.com>
2026-05-07 13:04:26 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -13,6 +13,8 @@ export default function App({ Component, pageProps }) {
         <meta name="mobile-web-app-capable" content="yes"/>
         <meta name="theme-color" content="#1e3a5f"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+    <link rel="icon" href="/icon.png" />
+<link rel="apple-touch-icon" href="/icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet"/>
