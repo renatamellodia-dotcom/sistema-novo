c6f31dd143e7cff02bef0db5379f3580bd643d8c
renatamellodia-dotcom <renatamellodia@gmail.com>
2026-05-08 07:55:28 -0300

    Update _app.js

--- _app.js
+++ _app.js
@@ -1,4 +1,16 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Bar do Complexo</title>
+        <link rel="icon" href="/icon.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
